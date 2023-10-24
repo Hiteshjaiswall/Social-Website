cb59248066f0ae2860d7d71a9e57cb23f6c5804d
hitesh jaiswal <108576039+Hiteshjaiswall@users.noreply.github.com>
2023-10-24 20:55:21 +0530

    making post schema and relatonship

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,7 +3,10 @@ module.exports.home=function(req, res){
 // this was to send the data now im rendering my page so i dont need it no more
     //return res.end("<h1>EXPRESS is up for social website</h1>")
 
-    return res.render('home.ejs', {
+    
+    
+    
+     return res.render('home.ejs', {
         title:"social"
     });
 }
