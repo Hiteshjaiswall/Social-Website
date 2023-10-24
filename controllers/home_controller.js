2d18e8417308d10b7230d097c9e928b2783a42e3
hitesh jaiswal <108576039+Hiteshjaiswall@users.noreply.github.com>
2023-10-25 02:51:36 +0530

    creting post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,37 @@
+
+
+//  lets display the posts on the screen for that i need to get all the posts
+//  require post
+
+const Post=require('../models/post');
+
 module.exports.home=function(req, res){
 
 // this was to send the data now im rendering my page so i dont need it no more
     //return res.end("<h1>EXPRESS is up for social website</h1>")
 
+    // Post.find({})
+    // .then(posts=>{
+    //     return res.render('home.ejs', {
+    //         title:"social",
+    //         posts:posts
+    //     });
+    // })
+    // .catch(err=>{
+    //     console.log("error in rendering the post", err);
+    // })
+
+    //  now we will populate the user 
+
+    Post.find({}).populate('user')
+    .then(posts=>{
+        return res.render('home.ejs', {
+            title:"social",
+            posts:posts
+        });
+    })
+    .catch(err=>{
+        console.log("error in rendering the post", err);
+    })
     
-    
-    
-     return res.render('home.ejs', {
-        title:"social"
-    });
 }
